237d089b91d7dce2ee77e091feae8ef23cca69d8
Ucas-AidenC <Aiden.challenger@ucas-edu.net>
2025-01-08 10:50:47 -0700

    made main function

---
+++ Fiancal Calculator/main.py
@@ -0,0 +1,8 @@
+#"Create a program that completes the following basic financial calculations:
+
+#How long it will take to save for a goal based on a weekly or monthly deposit
+#Compound Interest Calculator 
+#Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
+#Sale Price Calculator (apply discounts to prices)
+#Tip Calculator
+
