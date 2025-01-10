e8cb754d9155593abe415ad24783f2b790f98950
Ucas-AidenC <Aiden.challenger@ucas-edu.net>
2025-01-10 09:59:05 -0700

    finished saving and started selection

--- Fiancal Calculator/main.py
+++ Fiancal Calculator/main.py
@@ -1,3 +1,4 @@
+#Aiden Challenger Financial Calculator
 #"Create a program that completes the following basic financial calculations:
 
 #How long it will take to save for a goal based on a weekly or monthly deposit
@@ -5,4 +6,30 @@
 #Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
 #Sale Price Calculator (apply discounts to prices)
 #Tip Calculator
+
+#take goal+ deposit( seperate from and before selction )(don't forgor rounding)
+def main():
+    goal=int(input("How much money do you want to save?"))
+    deposit= int(input("How much can you deposit weekly?"))
+    savetime=(goal/deposit+"weeks")
+
+#ask user what they want to use(prob use words not numbers)(maybe have function for switching to ignore caps)(ensure it asks multiple times after last function completes) (i'm cooked)(lowk just make this the ian function)(bruh fix that ugle code) 
+def selection
+    use=input("What do you want to use     weekly goal saving compound intrest calculator Budget Allocator Sale Price Calculator Tip calculator  ")
+
+
+
+#compund equation
+
+
+
+
+# budget allocation(make function, ask for percentage wanted everytime)(find way to get tme into ask)(side function)
+def saving():
+    savings=int(input("How much do you save annually "))
+    entertainment=int(input("How much do you spend on entertainment"))
+    food=int(input("How much do you spend on food "))
+#sale price calc (apply discount to items make function) 
+
+# tip calc (add tip to prices)
 
