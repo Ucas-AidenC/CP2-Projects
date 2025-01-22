ab30f4f1ecfbc180232835b3362b9a90c5f3425b
Ucas-AidenC <aiden.challenger@ucas-edu.net>
2025-01-21 21:13:21 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+#Stores all items in a list
+#Function to add a new item
+#Function to search the items
+#Function to remove an item
+#Function that runs the code (displays the menu options inside and calls the functions inside of a while True loop)
+#Project must include
+#easy to understand variable and function names
+#Pseudocode comments explaining what the code is doing
+#Good use of white space to keep items separate and easy to read/find
+#Have at least 2 people test your code before submission!
+
+#List for books(editable)
+library_books=[]
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+  while true:
+    print("Personal Library Program")
+    print
+
+
+
+
+
