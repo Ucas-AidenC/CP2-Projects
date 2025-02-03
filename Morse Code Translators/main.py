d1866b6db27452e8fe7e4e9f836f4f3130c2f167
Ucas-AidenC <aiden.challenger@ucas-edu.net>
2025-02-03 10:51:09 -0700

    Create main.py
    started definf thin g

---
+++ Morse Code Translators/main.py
@@ -0,0 +1,19 @@
+english_alphabet
+morse_alphabet
+
+#i'm giving up
+
+def morse_to_english():
+
+
+
+def english_to_morse():
+  
+
+
+def main():
+  print("Morse code translators ")
+  print(Morse code to english translator")
+  print("english to morse code translator")
+  choice== int(input("what would you like to use")
+
