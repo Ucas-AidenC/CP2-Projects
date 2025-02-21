c7a9739e1866325f9b6c1c22926a88b6141140be
Ucas-AidenC <aiden.challenger@ucas-edu.net>
2025-02-21 10:50:11 -0700

    Update main.py

--- To Do List/main.py
+++ To Do List/main.py
@@ -1,1 +1,17 @@
 
+import csv
+#vasriables that should be updated and send update to 
+
+
+
+with open ("To Do List/To Do List.csv", "r") as file: 
+    data = 
+    #should save as a editable variable (dictiornat  9 )
+    #variable name here.uppdate = data 
+    
+
+
+#def add():
+#    with open ("To Do List/.csv", "r") as file: 
+ #       add_v = input("wha would you like to add to you to do list? ").strip()
+ #       file.write (“add_v”)
