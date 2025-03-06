a7b7ceaa93b8966c65d8fb591e7f9664a522f4e5
Ucas-AidenC <aiden.challenger@ucas-edu.net>
2025-03-05 23:59:00 -0700

    Update file_handling.py

--- Word Counter/file_handling.py
+++ Word Counter/file_handling.py
@@ -1,1 +1,34 @@
+#the file that will be called to handle the users files 
 
+from time_handling import *
+
+def read_file(filepath): 
+    with open(filepath, "r") as file:
+        return file.read()
+    
+def write_file(filepath, new_text):
+        # Read the existing content
+        with open(filepath, "r") as file:
+            existing_content = file.read()
+
+        updated_content = f"{existing_content}\n{new_text}" #combinign the new and old info 
+
+        # Calculate word count
+        word_count = len(updated_content.split())
+
+        # timestamp from otherr file 
+        timestamp = get_timestamp()
+
+        # Preparingg for final print 
+        final_content = f"{updated_content}\n\nWord Count: {word_count}\nLast Updated: {timestamp}"
+
+        # File Writing 
+        with open(filepath, "w") as file:
+            file.write(final_content)
+
+
+if __name__ == "__main__":
+    filepath = ""
+    new_text=""
+    read_file(filepath)
+    write_file(filepath, new_text)
